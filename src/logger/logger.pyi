f91f86120677d2d2f0b32b2e118633390d851645
Simon Watson <simon.watson.sjw@gmail.com>
2026-04-05 17:39:07 +1000

    [tidy] no stub needed for imports file

--- src/logger/logger.pyi
+++
@@ -1,4 +0,0 @@
-from .core import query_logs as query_logs, setup_logger as setup_logger
-from logging import CRITICAL as CRITICAL, DEBUG as DEBUG, ERROR as ERROR, INFO as INFO, Logger as Logger, WARNING as WARNING
-
-__all__ = ['setup_logger', 'query_logs', 'DEBUG', 'INFO', 'WARNING', 'CRITICAL', 'ERROR', 'Logger']
